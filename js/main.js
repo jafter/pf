34c714a79c89c4618c4e8ebe3f154c9249d6cec6
jafter <avrilsmile@naver.com>
2025-07-02 20:37:49 +0900

    250702_3

--- js/main.js
+++ js/main.js
@@ -40,44 +40,43 @@ $(function () {
         }
     }
     writeTxt()
-    const openDetail = (idx, title = '', text = '', isProfile = false) => {
-        $('.gallery').css({ 'display': 'none' })
-
-        if (!isProfile) {
-            $('.web_iframe').addClass('on').find('iframe').attr('src', `${videoSrc[idx]}`)
-            $('.play_interface').addClass('on')
-            $('.web_play').css({
-                'background': `url(images/image${idx + 1}_1.jpg) no-repeat center/cover`
-            })
-            $('.play_interface_title').css({
-                'background': `url(images/image${idx + 1}_title.jpg) no-repeat center 90%/cover`
-            })
-            $('.wrap_web .play_info').find('p').text(infoSrc[idx])
-            $('.play_top .title').find('p').text(title)
-            $('.wrap_web').addClass('on')
-
-            // ✅ 뒤로가기 히스토리 쌓기
-            history.pushState({ page: 'detail' }, '', '');
-        } else {
-            $('.profile_box_text').css({ 'bottom': '-50px' })
-            $('.web_iframe').addClass('on').find('iframe').attr('src', videoSrc)
-            $('.profile_interface').addClass('on')
-            $('.wrap_web .play_info').find('p').text('profiletext')
-            $('.play_top .title').find('p').text('Profile')
-            $('.wrap_web').addClass('on')
-            $('.profile_interface .icon').css({ 'display': 'flex' })
-
-            history.pushState({ page: 'profile' }, '', '');
-        }
-    }
-
     if (window.innerWidth <= 768) {
         $('.gallery li').each(function (index, el) {
             $(el).click(function () {
                 const title = $(this).find('h2').text()
                 const text = $(this).find('p').text()
                 const idx = $(this).index()
-                openDetail(idx, title, text, idx >= 5)
+                $('.gallery').css({ 'display': 'none' })
+                if (idx < 5) {
+                    $('.web_iframe').addClass('on')
+                    $('.web_iframe').find('iframe').attr('src', `${videoSrc[idx]}`)
+                    $('.play_interface').addClass('on')
+
+                    $('.web_play').css({
+                        'background': `url(images/image${idx + 1}_1.jpg) no-repeat center/cover`
+                    })
+                    $('.play_interface_title').css({
+                        'background': `url(images/image${idx + 1}_title.jpg) no-repeat center 90%/cover`
+                    })
+                    $('.wrap_web .play_info').find('p').text(infoSrc[idx])
+                    $('.play_top .title').hide()
+                    $('.wrap_web').addClass('on')
+                }
+                else {
+                    $('.web_iframe').find('iframe').attr('src', `${videoSrc[idx]}`)
+                    $('.web_iframe').addClass('on')
+                    $('.wrap_web .play_info').find('p').text('JINHOO LEE')
+                    $('.web_play').css({
+                        'background': `url(images/image${idx + 1}_1.jpg) no-repeat center/cover`
+                    })
+                    $('.wrap_web').addClass('on')
+                    $('.profile_interface .icon').css({
+                        'display': 'flex'
+                    })
+                    $('.mobile_profile').css({
+                        'left': '0'
+                    })
+                }
             })
         })
     } else {
@@ -86,51 +85,100 @@ $(function () {
                 const title = $(this).find('h2').text()
                 const text = $(this).find('p').text()
                 const idx = $(this).index()
-                openDetail(idx, title, text, false)
+                $('.gallery').css({ 'display': 'none' })
+                $('.web_iframe').find('iframe').attr('src', `${videoSrc[idx]}`)
+                $('.play_interface').addClass('on')
+                $('.web_play').css({
+                    'background': `url(images/image${idx + 1}_1.jpg) no-repeat center/cover`
+                })
+                $('.play_interface_title').css({
+                    'background': `url(images/image${idx + 1}_title.jpg) no-repeat center 90%/cover`
+                })
+                $('.wrap_web .play_info').find('p').text(infoSrc[idx])
+                $('.play_top .title').find('p').text(title)
+                $('.wrap_web').addClass('on')
+                $('.web_iframe').find('iframe').attr('src', `${videoSrc[idx]}`)
+                $('.play_interface').addClass('on')
+
+                $('.profile_box_text').css({ 'bottom': '-50px' })
+                $('.web_play').css({
+                    'background': `url(images/image${idx + 1}_1.jpg) no-repeat center/cover`
+                })
+                $('.play_interface_title').css({
+                    'background': `url(images/image${idx + 1}_title.jpg) no-repeat center 90%/cover`
+                })
+                $('.wrap_web .play_info').find('p').text(infoSrc[idx])
+                $('.play_top .title').find('p').text(title)
+                $('.wrap_web').addClass('on')
             })
         })
     }
-
     $('.profile_box_text').click(function () {
-        const videoSrc = 'https://www.youtube.com/embed/qvZutqHp5RU?si=8zs6tySga6ZuTMaQ?autoplay=1';
-        openDetail(5, 'Profile', '', true);
-    });
-
-    // ✅ 공통 닫기 함수 (.close 동작을 흉내냄)
-    function closeDetail() {
+        const videoSrc = 'https://www.youtube.com/embed/qvZutqHp5RU?si=8zs6tySga6ZuTMaQ?autoplay=1'
+        $('.gallery').css({ 'display': 'none' })
+        $('.profile_box_text').css({ 'bottom': '-50px' })
+        $('.web_iframe').find('iframe').attr('src', videoSrc)
+        $('.profile_interface').addClass('on')
+        $('.wrap_web .play_info').find('p').text('profiletext')
+        $('.play_top2').removeClass('on')
+        $('.web_play').css({
+            'background': 'url(images/image6_1.jpg) no-repeat center/cover'
+        })
+        $('.play_top .title').find('p').text('Profile')
+        $('.wrap_web').addClass('on')
+        $('.profile_interface .icon').css({
+            'display': 'flex'
+        })
+    })
+    $('.play_top .logo, .play_top .close, .play_top .title').click(function () {
         var iframe = $('#video');
         var currentSrc = iframe.attr('src');
         if (currentSrc.includes('autoplay=1')) {
             var newSrc = currentSrc.replace('autoplay=1', 'autoplay=0');
             iframe.attr('src', newSrc);
         }
-        iframe.attr('src', currentSrc.replace('autoplay=1', 'autoplay=0'));
-
+        var newSrc = currentSrc.replace('autoplay=1', 'autoplay=0');
+        iframe.attr('src', newSrc);
         $('.wrap_web').removeClass('on')
         $('.profile_box_text').css({ 'bottom': '0' })
         $('.play_interface').removeClass('on')
         $('.profile_interface').removeClass('on')
         $('.gallery').css({ 'display': 'flex' })
-        $('.mobile_profile').css({ 'left': '-110%' })
-    }
-
-    // ✅ 닫기 버튼 클릭 이벤트
-    $('.play_top .logo, .play_top .close, .play_top .title').click(closeDetail);
-    $('.play_top2 .logo, .play_top2 .close').click(closeDetail);
-    $('.play_top3 .logo, .play_top3 .close').click(closeDetail);
-
-    // ✅ 뒤로가기 시 .close 동작 실행
-    window.onpopstate = function (event) {
-        closeDetail();
-    };
-
+        $('.mobile_profile').css({
+            'left': '-110%'
+        })
+    })
     $('.play_btn').click(function () {
         $('.web_iframe').addClass('on')
         $('.play_top2').addClass('on')
     })
-
+    $('.play_top2 .logo, .play_top2 .close').click(function () {
+        var iframe = $('#video');
+        var currentSrc = iframe.attr('src');
+        if (currentSrc.includes('autoplay=1')) {
+            var newSrc = currentSrc.replace('autoplay=1', 'autoplay=0');
+            iframe.attr('src', newSrc);
+        }
+        $('.play_top2').removeClass('on')
+        $('.web_iframe').removeClass('on')
+        var newSrc = currentSrc.replace('autoplay=1', 'autoplay=0');
+        iframe.attr('src', newSrc);
+    })
     $('.profile_interface').click(function () {
         $('.web_iframe').addClass('on')
         $('.play_top3').addClass('on')
+    })
+    $('.play_top3 .logo, .play_top3 .close').click(function () {
+        var iframe = $('#video');
+        var currentSrc = iframe.attr('src');
+        if (currentSrc.includes('autoplay=1')) {
+            var newSrc = currentSrc.replace('autoplay=1', 'autoplay=0');
+            iframe.attr('src', newSrc);
+        }
+        $('.play_top3').removeClass('on')
+        $('.web_iframe').removeClass('on')
+        $('.profile_interface').addClass('on')
+        var newSrc = currentSrc.replace('autoplay=1', 'autoplay=0');
+        iframe.attr('src', newSrc);
     })
 })
